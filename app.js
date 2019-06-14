4b8d74e6661437fee2af3b91ada85408d9c5c441
Nouman Khan <nomi_174@hotmail.com>
2019-06-14 23:53:59 +0500

    Added _res array holding res objects of /spin requests. and generates a response when socket.on is executed

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
 const express = require("express");
-const sleep = require("sleep");
 const app = express();
+// const sleep = require("sleep");
 var _http = require('http');
 var http = require('http').Server(app);
 var io = require('socket.io')(http);
@@ -8,23 +8,46 @@ var io = require('socket.io')(http);
 app.use(express.static('public'));
 
 var _result = 'No result';
+var _res = [];
 
 http.listen(3000, function () {
     console.log('Check the Wheek on http://localhost:3000');
 });
 
+// Run once, on first time (to init socket)
 io.on('connection', function (socket) {
+    // Run everytime after the result is generated
     socket.on('spin', function (msg) {
+        console.log("Here now!")
         _result = msg;
-        console.log(_result);
+        console.log("Result: ", _result);
+        _res[_res.length-1].status(200).send({ message: _result });
+        _res = []
     });
 });
 
 app.get('/spin', (req, res) => {
+    _res.push(res)
     io.emit('spin', '');
-    res.status(200).send({ message: 'Wheel Spinned' });
+    console.log("Request generated to spin!") 
+
+    // res.status(200).send({ message: 'Wheel Spinned' });
 });
 
 app.get('/result', (req, res) => {
+    
+    // if(_result != "No result") {
+        // } else {
+            //     delay(1000)
+            // }
+            
     res.status(200).send({ message: _result });
 });
+
+
+const delay = (ms) => {
+    const startPoint = new Date().getTime()
+    while (new Date().getTime() - startPoint <= ms) {console.log("In delay")}
+}
+
+
